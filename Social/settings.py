aba80fa2d82a8c6c8b55e0897a028d71ae61dad1
Captainbrooks <138337847+Captainbrooks@users.noreply.github.com>
2024-11-20 01:29:20 -0400

    Redis cloud with render configured

--- Social/settings.py
+++ Social/settings.py
@@ -108,7 +108,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("rediss://red-cspm4gdumphs73d4rnmg:clnM8Gn9USvXaNQLpqKnJATi6ADtHROX@oregon-redis.render.com:6379")],
         },
     },
 }
